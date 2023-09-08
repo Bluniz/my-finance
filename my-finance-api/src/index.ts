dc1f88de90f30760bdfd0c3e3d92b205133c2341
Lucas <lucassantosrosa51@gmail.com>
2023-09-08 20:06:47 -0300

    chore: add cors without initial configs

--- my-finance-api/src/index.ts
+++ my-finance-api/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import { router } from "./routes";
 
+// var corsOptions = {
+//   origin: "http://example.com",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
